4f3af6281e570745bd6f48c3f1ce3836b804c931
Yash Ghatti <yash.ghatti@fcc-fac.ca>
2022-04-17 15:49:19 -0600

    test package and deploy

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import {WidgetComponent} from "../widget/widget.component";
 
 
 @NgModule({
@@ -14,6 +15,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, WidgetComponent]
 })
 export class HomePageModule {}
